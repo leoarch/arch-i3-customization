6d2eb14bd13dd7a1b025029532a9984e177da628
leoarch <43377227+leoarch@users.noreply.github.com>
2018-11-03 13:17:52 -0300

    Add files via upload

--- i3-customization.sh
+++ i3-customization.sh
@@ -8,7 +8,8 @@
 #===================================================#
 
 # instalando yay (AUR)
-cd ~/
+echo -e "${_g}===> Instalando yay (AUR)${_o}"; sleep 1
+cd ${HOME}
 mkdir yaygit
 cd yaygit
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz 
@@ -20,50 +21,63 @@ yay -S polybar ttf-font-awesome-4 --noconfirm
 echo -e "${_g}===> Baixar os arquivos da configuração e acessando os arquivos de configurações${_o}"; sleep 1
 git clone https://github.com/jirrezdex/archlinux-i3.git && cd archlinux-i3
 
+# i3
 echo -e "${_g}===> Mover os arquivos do ~/.config/i3 e dar permissão${_o}"; sleep 1
-if [-d "${HOME}/.config/dunst"]; then
+
+if [ -d "${HOME}/.config/i3"]; then
 	rm -dR ~/.config/i3
-else
-	mv i3/ ~/.config
-	chmod +x ~/.config/i3/basico.sh
 fi
 
+mv i3/ ~/.config
+chmod +x ~/.config/i3/basico.sh
+
+# polybar
 echo -e "${_g}===> Mover os arquivos do ~/.config/polybar e dar permissão${_o}"; sleep 1
-if [-d "${HOME}/.config/dunst"]; then
+
+if [ -d "${HOME}/.config/polybar" ]; then
 	sudo rm -dR ~/.config/polybar
-else
-	mv polybar/ ~/.config
-	chmod +x ~/.config/polybar/launch.sh
 fi
 
+mv polybar/ ~/.config
+chmod +x ~/.config/polybar/launch.sh
+
+# dunst
 echo -e "${_g}===> Mover os arquivos do ~/.config/dunst${_o}"; sleep 1
-if [-d "${HOME}/.config/dunst"]; then
+
+if [ -d "${HOME}/.config/dunst" ]; then
 	sudo rm -dR ~/.config/dunst
-else
-	mv dunst/ ~/.config
 fi
 
+mv dunst/ ~/.config
+
+# rofi
 echo -e "${_g}===> Mover os arquivos do ~/.config/rofi${_o}"; sleep 1
-if [-d "${HOME}/.config/dunst"]; then
+
+if [ -d "${HOME}/.config/rofi" ]; then
 	sudo rm -dR ~/.config/rofi
-else
-	mv rofi/ ~/.config
-	mv .dex.rasi ~
 fi
 
+mv rofi/ ~/.config
+mv .dex.rasi ~
+
+# imagens
 echo -e "${_g}===> Mover os arquivos do ~/Imagens${_o}"; sleep 1
-if [-d "${HOME}/Imagens"]; then
-	if [-e "${HOME}/Imagens/W1.png"]; then
-		sudo rm -dR ~/Imagens/W1.png
-	fi
-	mv W1.png ~/Imagens
-else
-	mkdir ~/Imagens
-	mv W1.png ~/Imagens
+
+if [ -d "${HOME}/Imagens" ]; then
+	sudo rm -dR ~/Imagens
 fi
 
+mkdir ~/Imagens
+sudo rm -dR ~/Imagens/W1.png
+mv W1.png ~/Imagens
+
+# termite
 echo -e "${_g}===> Mover os arquivos do /etc/xdg/termite${_o}"; sleep 1
-sudo rm -dR /etc/xdg/termite
+
+if [ -d "/etc/xdg/termite" ]; then
+	sudo rm -dR /etc/xdg/termite
+fi
+
 sudo mv termite/ /etc/xdg/
 
 echo -e "${_g}===> Criar diretórios necessários${_o}"; sleep 1
